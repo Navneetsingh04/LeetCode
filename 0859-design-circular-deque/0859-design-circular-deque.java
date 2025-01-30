2e0257b86426435dc58b93b08baa357ff85eab4e
Navneet Singh <118204480+Navneetsingh04@users.noreply.github.com>
2025-01-30 19:46:44 +0530

    Time: 4 ms (100%), Space: 44.9 MB (45.54%) - LeetHub

---
+++ 0859-design-circular-deque/0859-design-circular-deque.java
@@ -0,0 +1,88 @@
+class MyCircularDeque {
+    public int[] deq;
+    public int k;
+    public int front;
+    public int rear;
+    public int currCount;
+
+    public MyCircularDeque(int k) {
+        this.k = k;
+        deq = new int[k];
+        front = 0;
+        rear = k-1;
+        currCount = 0;
+    }
+    
+    public boolean insertFront(int value) {
+        if(isFull()){
+            return false;
+        }
+        front = (front-1+k)%k;
+        deq[front] = value;
+        currCount++;
+        return true;
+    }
+    
+    public boolean insertLast(int value) {
+        if(isFull()){
+            return false;
+        }
+        rear = (rear+1)%k;
+        deq[rear] = value;
+        currCount++;
+        return true;
+    }
+    
+    public boolean deleteFront() {
+        if(isEmpty()){
+            return false;
+        }
+        front = (front+1)%k;
+        currCount--;
+        return true;
+    }
+    
+    public boolean deleteLast() {
+        if(isEmpty()){
+            return false;
+        }
+        rear = (rear-1+k)%k;
+        currCount--;
+        return true;
+    }
+    
+    public int getFront() {
+        if(isEmpty()){
+            return -1;
+        }
+        return deq[front];
+    }
+    
+    public int getRear() {
+        if(isEmpty()){
+            return -1;
+        }
+        return deq[rear];
+    }
+    
+    public boolean isEmpty() {
+        return currCount == 0;
+    }
+    
+    public boolean isFull() {
+        return currCount == k;
+    }
+}
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque obj = new MyCircularDeque(k);
+ * boolean param_1 = obj.insertFront(value);
+ * boolean param_2 = obj.insertLast(value);
+ * boolean param_3 = obj.deleteFront();
+ * boolean param_4 = obj.deleteLast();
+ * int param_5 = obj.getFront();
+ * int param_6 = obj.getRear();
+ * boolean param_7 = obj.isEmpty();
+ * boolean param_8 = obj.isFull();
+ */
