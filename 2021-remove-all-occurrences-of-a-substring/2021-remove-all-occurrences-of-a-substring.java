0703775ea99d5b37783b437353f00a40216b309a
Navneet Singh <118204480+Navneetsingh04@users.noreply.github.com>
2025-01-31 18:30:30 +0530

    Time: 1 ms (99.49%), Space: 42.2 MB (79.76%) - LeetHub

---
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        int n = s.length();
+        StringBuilder ans = new StringBuilder(s);
+        while(s.contains(part)){
+          int i = ans.indexOf(part);
+          if(i == -1){
+            return s;
+          }
+          ans = ans.replace(i,i+part.length(),"");
+          s = ans.toString();
+        }
+        return s;
+    }
+}
