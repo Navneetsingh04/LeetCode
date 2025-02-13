6c548ee5d32d996851b2f0a04352d0abda767098
Navneet Singh <118204480+Navneetsingh04@users.noreply.github.com>
2025-02-13 20:37:02 +0530

    Time: 2 ms (68.03%), Space: 43.1 MB (51.76%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -3,12 +3,12 @@ class Solution {
         int n = s.length();
         StringBuilder ans = new StringBuilder(s);
         while(s.contains(part)){
-          int i = ans.indexOf(part);
-          if(i == -1){
-            return s;
-          }
-          ans = ans.replace(i,i+part.length(),"");
-          s = ans.toString();
+            int i = ans.indexOf(part);
+            if(i == -1){
+                return s;
+            }
+            ans = ans.replace(i,i+part.length(),"");
+            s = ans.toString();
         }
         return s;
     }
