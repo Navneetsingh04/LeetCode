a25592ebf5f662e18cb5d8bd18454fc1b98a8665
Navneet Singh <118204480+Navneetsingh04@users.noreply.github.com>
2025-03-01 10:56:18 +0530

    Time: 1 ms (96.37%), Space: 43.4 MB (97.15%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -1,21 +1,22 @@
 class Solution {
-    public void swap(int nums[],int i,int j){
+    public void swap(int nums[], int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
+
     public int[] applyOperations(int[] nums) {
         int n = nums.length;
-        for(int i = 0;i<n-1;i++){
-            if(nums[i] == nums[i+1]){
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
                 nums[i] *= 2;
-                nums[i+1] = 0;
+                nums[i + 1] = 0;
             }
         }
         int pos = 0;
-        for(int i = 0;i<n;i++){
-            if(nums[i] != 0){
-                swap(nums,i,pos);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                swap(nums, i, pos);
                 pos++;
             }
         }
