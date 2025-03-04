366e67554941e65443ec1f0545d7e18871748902
Navneet Singh <118204480+Navneetsingh04@users.noreply.github.com>
2025-03-04 20:50:56 +0530

    Time: 0 ms (100%), Space: 40.3 MB (78.49%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,10 +1,17 @@
 class Solution {
     public boolean checkPowersOfThree(int n) {
+        int p = 0;
+        while(Math.pow(3,p) <= n){
+            p++;
+        }
         while(n>0){
-            if(n%3 == 2){
+            if(n >= Math.pow(3,p)){
+                n = n - (int)Math.pow(3,p);
+            }
+            if(n >= Math.pow(3,p)){
                 return false;
             }
-            n = n/3;
+            p--;
         }
         return true;
     }
