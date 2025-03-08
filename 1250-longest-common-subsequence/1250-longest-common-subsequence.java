d536b31dd5c0c2a7b4c9b8a0efaf9b62d981db4c
Navneet Singh <118204480+Navneetsingh04@users.noreply.github.com>
2025-03-08 20:58:15 +0530

    Time: 24 ms (33.11%), Space: 50.8 MB (66.75%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solve(int index1,int index2,String s1, String s2,int dp[][]){
-        if(index1 == 0 || index2 == 0){
+        if(index1 < 0 || index2 <0){
             return 0;
         }
         if(dp[index1][index2] != -1){
             return dp[index1][index2];
         }
-        if(s1.charAt(index1-1) == s2.charAt(index2-1)){
+        if(s1.charAt(index1) == s2.charAt(index2)){
             return 1+solve(index1-1,index2-1,s1,s2,dp);
         }
         dp[index1][index2] =  0+Math.max(solve(index1-1,index2,s1,s2,dp),solve(index1,index2-1,s1,s2,dp));
@@ -19,6 +19,6 @@ class Solution {
         for(int row[] : dp){
             Arrays.fill(row,-1);
         }
-        return solve(n,m,text1,text2,dp);
+        return solve(n-1,m-1,text1,text2,dp);
     }
 }
