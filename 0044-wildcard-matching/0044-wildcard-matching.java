14f1fb32699adfa797b0b6a241b5f741344ca34b
Navneet Singh <118204480+Navneetsingh04@users.noreply.github.com>
2025-03-16 00:36:21 +0530

    Time: 18 ms (63.73%), Space: 49 MB (8.71%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -3,14 +3,14 @@ class Solution {
         int n = s.length();
         int m = p.length();
         Boolean dp[][] = new Boolean[n+1][m+1];
-        return solve(n-1,m-1,s,p,dp);
+        return solve(n,m,s,p,dp);
     }
     public boolean solve(int i,int j,String s,String p,Boolean dp[][]){
-        if(i<0 && j<0) return true;
-        if(i>=0 && j<0) return false;
-        if(i<0 && j>=0){
-            for(int k = 0;k<=j;k++){
-                if(p.charAt(k) != '*'){
+        if(i==0 && j==0) return true;
+        if(i>0 && j==0) return false;
+        if(i==0 && j>0){
+            for(int k = 1;k<=j;k++){
+                if(p.charAt(k-1) != '*'){
                     return false;
                 }
             }
@@ -19,10 +19,10 @@ class Solution {
         if(dp[i][j] != null){
             return dp[i][j];
         }
-        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?'){
+        if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?'){
             return dp[i][j] = solve(i-1,j-1,s,p,dp);
         }
-        if(p.charAt(j) == '*'){
+        if(p.charAt(j-1) == '*'){
             return dp[i][j] = solve(i-1,j,s,p,dp) || solve(i,j-1,s,p,dp);
         }
         return dp[i][j] = false;
