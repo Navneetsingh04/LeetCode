7f929868bfb45178cfa7f86a3d90244a5c06b039
Navneet Singh <118204480+Navneetsingh04@users.noreply.github.com>
2025-03-17 16:35:55 +0530

    Time: 1 ms (79.97%), Space: 41.4 MB (94.43%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,28 +1,21 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int dp[][] = new int[n+1][2];
-        for(int i[] :  dp){
-            Arrays.fill(i,-1);
+        int dp[][] = new int[n+2][2];
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+        for(int i = n-1;i>=0;i--){
+            for(int buy = 0;buy<=1;buy++){
+                if(buy == 1){
+                    dp[i][buy] = Math.max((-prices[i]+dp[i+1][0]),
+                    0+dp[i+1][1]);
+                }
+                else{
+                    dp[i][buy] = Math.max((prices[i]+dp[i+2][1]),
+                    0+dp[i+1][0]);
+                }
+            }
         }
-        return solve(0,1,prices,dp,n);
-    }
-    public int solve(int i,int buy,int prices[],int dp[][],int n){
-        if(i >= n) return 0;
-        int profit = 0;
-        if(dp[i][buy] != -1){
-            return dp[i][buy];
-        }
-        if(buy == 1){
-            int take = -prices[i]+solve(i+1,0,prices,dp,n);
-            int notTake = 0+solve(i+1,1,prices,dp,n);
-            profit = Math.max(take,notTake);
-        }
-        else{
-            int take = prices[i]+solve(i+2,1,prices,dp,n);
-            int notTake = 0+solve(i+1,0,prices,dp,n);
-            profit = Math.max(take,notTake);
-        }
-        return dp[i][buy] = profit;
+        return dp[0][1];
     }
 }
