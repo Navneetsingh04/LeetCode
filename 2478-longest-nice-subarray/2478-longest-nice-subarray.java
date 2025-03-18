d540a694ddabef82afa99084834f3a435f0c891d
Navneet Singh <118204480+Navneetsingh04@users.noreply.github.com>
2025-03-18 18:24:05 +0530

    Time: 3 ms (98.64%), Space: 56 MB (33.79%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.java
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.java
@@ -1,26 +1,15 @@
 class Solution {
-    public boolean isNice(int nums[],int i,int j){
-        int mask =0;
-        for(int k = i;k<=j;k++){
-            if((mask & nums[k]) != 0){
-                return false;
-            }
-            mask |= nums[k];
-        }
-        return true;
-    }
     public int longestNiceSubarray(int[] nums) {
         int n = nums.length;
         int count = 0;
+        int left = 0,bitmask = 0;
         for(int i = 0;i<n;i++){
-            for(int j = i;j<n;j++){
-                if(isNice(nums,i,j)){
-                    count = Math.max(count,j-i+1);
-                }
-                else{
-                    break;
-                }
+            while((bitmask & nums[i]) != 0){
+                bitmask ^= nums[left];
+                left++;
             }
+            bitmask |= nums[i];
+            count = Math.max(count,i-left+1);
         }
         return count;
     }
